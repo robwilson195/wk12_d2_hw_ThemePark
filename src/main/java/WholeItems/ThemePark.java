b8be4627c7f4fc67d45c7eb813cfa7432a14fa05
Rob Wilson <rob.wilson195@gmail.com>
2019-01-15 23:10:59 +0000

    Started fleshing out the ThemePark class, including adding attractions of different subclasses.

--- src/main/java/WholeItems/ThemePark.java
+++ src/main/java/WholeItems/ThemePark.java
@@ -1,4 +1,40 @@
 package WholeItems;
 
+import Attractions.Attraction;
+import Attractions.RollerCoaster;
+import People.Visitor;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private String name;
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Visitor> visitors;
+
+    public ThemePark(String name) {
+        this.name = name;
+        this.attractions = new ArrayList<>();
+        this.visitors = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Attraction> getAttractions() {
+        return attractions;
+    }
+
+    public ArrayList<Visitor> getVisitors() {
+        return visitors;
+    }
+
+    public int attractionCount() {
+        return attractions.size();
+    }
+
+    public void addAttraction(Attraction attraction) {
+        attractions.add(attraction);
+    }
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,2 +1,52 @@
+import Attractions.Attraction;
+import Attractions.Playground;
+import Attractions.RollerCoaster;
+import People.Visitor;
+import WholeItems.ThemePark;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ThemeParkTest {
+
+    private Visitor visitor1;
+    private RollerCoaster attraction1;
+    private Playground attraction2;
+    private ThemePark themePark;
+
+    @Before
+    public void setUp() {
+        attraction1 = new RollerCoaster("Smiler", 12, 140, 4.80, 4);
+        attraction2 = new Playground("Sunshine", 4, 30, 1.00, 3, 15);
+        themePark = new ThemePark("Rob's Fun Bonanza");
+        visitor1 = new Visitor(16, 160, 40.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Rob's Fun Bonanza", themePark.getName());
+    }
+
+    @Test
+    public void startsNoAttractions() {
+        assertEquals(0, themePark.attractionCount());
+    }
+
+    @Test
+    public void canAddAttraction() {
+        themePark.addAttraction(attraction1);
+        assertEquals(1, themePark.attractionCount());
+    }
+
+    @Test
+    public void canAddAttractionsOfDifferentTypes() {
+        themePark.addAttraction(attraction1);
+        themePark.addAttraction(attraction2);
+        assertEquals(2, themePark.attractionCount());
+    }
+
+    @Test
+    public void canAddVisitors() {
+    }
 }
